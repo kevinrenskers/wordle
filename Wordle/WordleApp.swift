38ed20ded1fcb0434c974cdb0ef6f24ca117addc
Kevin Renskers <kevin@loopwerk.io>
2022-02-25 09:58:08 +0100

    Initial Commit

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Kevin Renskers on 25/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
